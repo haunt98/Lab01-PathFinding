36b96f8067f194a0a6f80ab641b6e152f52b06d4
Tran Hau <ngtranhau@gmail.com>
2018-10-09 16:41:12 +0700

    Simple solve with A* from wiki, add output solution to a file

--- Source/lab02.py
+++ Source/lab02.py
@@ -1,3 +1,5 @@
+# TODO write test, more test !!
+
 # for arguments
 import sys
 
@@ -5,43 +7,32 @@ import math
 
 
 # Point object to show where in the map
+# also cost and parent
 class Point:
     def __init__(self, row, col):
         self.row = row
         self.col = col
+        self.parent = None
+        self.cost = 0
 
     def EuclideanDistance(self, point):
         return math.sqrt((point.row - self.row) * (point.row - self.row) +
                          (point.col - self.col) * (point.col - self.col))
 
-    def isSame(self, point):
+    def isSamePos(self, point):
         return self.row == point.row and self.col == point.col
 
-
-# PointMore object not only show where in the mao
-# but also show its parent, its cost
-class PointMore:
-    def __init__(self, point):
-        self.point = point
-        self.parent = None
-        self.cost = 0
-
-    def setParent(self, parent):
-        self.parent = parent
-
-    def setCost(self, cost):
-        self.cost = cost
-
-    def addCost(self, moreCost):
-        self.cost += moreCost
-
     def print(self):
-        print(self.point.row, self.point.col, self.cost)
+        print(self.row, self.col, self.cost)
+
+    # f(n) = cost(n) + heuristic(n)
+    def calc_f(self, goalPoint):
+        return self.cost + self.EuclideanDistance(goalPoint)
 
 
-# PriorityQueue object for store PointMore
-# lower cost mean higher priority to be pulled out
-class PriorityQueue:
+# AStarList object for store Point
+# use f(n) = cost(n) + heuristic(n)
+class AStarList:
     def __init__(self):
         self.data = []
 
@@ -50,31 +41,39 @@ class PriorityQueue:
             i.print()
 
     def isEmpty(self):
-        # empty sequences are false
+        # empty sequences are False
         return not self.data
 
-    def add(self, newPointMore):
-        # only add new PointMore if not exist in data
-        # if exist, only replace of new PointMore has lower cost
-        exist = False
-        for pointMore in self.data:
-            if pointMore.point.isSame(newPointMore.point):
-                exist = True
-                if pointMore.cost > newPointMore.cost:
-                    pointMore.setCost(newPointMore.cost)
-                break
-        if not exist:
-            self.data.append(newPointMore)
+    def add(self, newPoint):
+        self.data.append(newPoint)
 
-    def pullLowest(self):
+    # only replace if replacePoint has lower cost()
+    def replace(self, replacePoint):
+        for point in self.data:
+            if point.isSamePos(replacePoint):
+                if point.cost > replacePoint.cost:
+                    point.cost = replacePoint.cost
+                    point.parent = replacePoint.parent
+                break
+
+    # return Point with lowest f()
+    def remove(self, goalPoint):
         if self.isEmpty():
             return
-        # find PointMore which has lowest cost to be pulled
-        lowestCostIndex = 0
+        minIndex = 0
         for i in range(1, len(self.data)):
-            if self.data[i].cost < self.data[lowestCostIndex].cost:
-                lowestCostIndex = i
-        return self.data.pop(lowestCostIndex)
+            if self.data[i].calc_f(goalPoint) < self.data[minIndex].calc_f(
+                    goalPoint):
+                minIndex = i
+        return self.data.pop(minIndex)
+
+    # return True if findPoint exist
+    # otherwise return False
+    def exist(self, findPoint):
+        for point in self.data:
+            if point.isSamePos(findPoint):
+                return True
+        return False
 
 
 class PathMap:
@@ -89,7 +88,7 @@ class PathMap:
         for line in f:
             row = []
             for word in line.split():
-                row.append(int(word))
+                row.append(str(word))
             self.data.append(row)
 
     def printSize(self):
@@ -111,53 +110,79 @@ class PathMap:
                 return False
         return True
 
-    # return true if point in map and not obstacle
-    # otherwise return false
-    def isMovablePoint(self, point):
-        # check if point in map
-        if point.row < 0 or point.row >= self.size or point.col < 0 or point.col >= self.size:
-            return False
-        # check if point is obstacle
-        if self.data[point.row][point.col] == 1:
-            return False
-        return True
+    # return True if Point is in map
+    # otherwise return False
+    def isValidPoint(self, point):
+        return point.row >= 0 and point.row < self.size and point.col >= 0 and point.col < self.size
 
-    # next move of p is point 1 -> 8
-    # valid move if next point is in map and not obstacle
-    # return list of valid next move
+    # return True if Point is obstacle
+    # otherwise return False
+    def isObstaclePoint(self, point):
+        return self.data[point.row][point.col] == '1'
+
+    def isMovablePoint(self, point):
+        return self.isValidPoint(point) and not self.isObstaclePoint(point)
+
+    # next move of p is Point from 1 to 8
+    # valid move if next Point is in map and not obstacle
+    # return list Point object
     # 1 2 3
     # 8 p 4
     # 7 6 5
     def nextMove(self, point):
-        listNextMove = []
+        nextPointS = []
         # 1 2 3
         if self.isMovablePoint(Point(point.row - 1, point.col - 1)):
-            listNextMove.append(Point(point.row - 1, point.col - 1))
+            nextPoint = Point(point.row - 1, point.col - 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         if self.isMovablePoint(Point(point.row - 1, point.col)):
-            listNextMove.append(Point(point.row - 1, point.col))
+            nextPoint = Point(point.row - 1, point.col)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         if self.isMovablePoint(Point(point.row - 1, point.col + 1)):
-            listNextMove.append(Point(point.row - 1, point.col + 1))
+            nextPoint = Point(point.row - 1, point.col + 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         # 4
         if self.isMovablePoint(Point(point.row, point.col + 1)):
-            listNextMove.append(Point(point.row, point.col + 1))
+            nextPoint = Point(point.row, point.col + 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         # 5 6 7
         if self.isMovablePoint(Point(point.row + 1, point.col + 1)):
-            listNextMove.append(Point(point.row + 1, point.col + 1))
+            nextPoint = Point(point.row + 1, point.col + 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         if self.isMovablePoint(Point(point.row + 1, point.col)):
-            listNextMove.append(Point(point.row + 1, point.col))
+            nextPoint = Point(point.row + 1, point.col)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         if self.isMovablePoint(Point(point.row + 1, point.col - 1)):
-            listNextMove.append(Point(point.row + 1, point.col - 1))
+            nextPoint = Point(point.row + 1, point.col - 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
         # 8
         if self.isMovablePoint(Point(point.row, point.col - 1)):
-            listNextMove.append(Point(point.row, point.col - 1))
-        return listNextMove
+            nextPoint = Point(point.row, point.col - 1)
+            nextPoint.parent = point
+            nextPoint.cost = point.cost + 1
+            nextPointS.append(nextPoint)
+        return nextPointS
 
     def printNextMove(self, point):
         for move in self.nextMove(point):
-            print(move.row, move.col)
+            move.print()
 
 
-class PathProblem:
+class FindingPathProblem:
     # get value from file
     # size
     # start_row,start_col
@@ -185,6 +210,97 @@ class PathProblem:
         print(self.goalPoint.row, self.goalPoint.col)
         self.pathMap.printMap()
 
+    def isValid(self):
+        if not self.pathMap.isValid():
+            return False
+        if not self.pathMap.isValidPoint(
+                self.startPoint) or not self.pathMap.isValidPoint(
+                    self.goalPoint):
+            return False
+        return True
+
+    # https://en.wikipedia.org/wiki/A*_search_algorithm
+    # assume heuristic(n) is consistent
+    # otherwise, must rediscover point in closeList
+    def solveAStar(self):
+        # check path map again for size
+        if not self.isValid():
+            print(
+                'Input path map wrong size or start, goal point wrong position'
+            )
+            return
+
+        # init closeList and openList
+        closeList = AStarList()
+        openList = AStarList()
+        openList.add(self.startPoint)
+
+        while not openList.isEmpty():
+            point = openList.remove(self.goalPoint)
+
+            # found goal
+            if point.isSamePos(self.goalPoint):
+                return point
+
+            # move point from openList to closeList
+            openList.remove(point)
+            closeList.add(point)
+
+            for nextPoint in self.pathMap.nextMove(point):
+                # nextPoint already close
+                if closeList.exist(nextPoint):
+                    continue
+                # nextPoint not yet open
+                elif not openList.exist(nextPoint):
+                    openList.add(nextPoint)
+                # nextPoint exist in openList
+                # replace if nextPoint has lower cost
+                else:
+                    openList.replace(nextPoint)
+
+    def getSolutionPath(self, goalPoint):
+        solutionPath = []
+        if goalPoint == None:
+            return
+        p = goalPoint
+        while p.parent != None:
+            solutionPath.insert(0, p)
+            p = p.parent
+        solutionPath.insert(0, p)
+        return solutionPath
+
+    def solution(self, f):
+        goalPoint = self.solveAStar()
+        if goalPoint == None:
+            f.write('-1\n')
+            return
+        # cost to goal
+        f.write(str(goalPoint.cost) + '\n')
+
+        # path to goal
+        for point in self.getSolutionPath(goalPoint):
+            f.write('(' + str(point.row) + ',' + str(point.col) + ') ')
+        f.write('\n')
+
+        # create solution map
+        solutionMap = []
+        for row in self.pathMap.data:
+            solutionMap.append(row)
+        for row in solutionMap:
+            for col in row:
+                if col == 0:
+                    col = '-'
+                elif col == 1:
+                    col = 'o'
+        solutionMap[self.startPoint.row][self.startPoint.col] = 'S'
+        solutionMap[goalPoint.row][goalPoint.col] = 'G'
+        for point in self.getSolutionPath(goalPoint):
+            solutionMap[point.row][point.col] = 'x'
+        for row in solutionMap:
+            for col in row:
+                f.write(col + ' ')
+            f.write('\n')
+
 
 # return file if open success
 # if fail, return None
@@ -210,7 +326,8 @@ def main():
     if f_out == None:
         f_in.close()
 
-    pathProblem = PathProblem(f_in)
+    findingPathProblem = FindingPathProblem(f_in)
+    findingPathProblem.solution(f_out)
 
     # must close file after return
     f_in.close()
