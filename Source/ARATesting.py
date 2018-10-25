ddc5342a087ec62ad922f3208a24fbdb5274f647
Tran Hau <ngtranhau@gmail.com>
2018-10-25 08:11:12 +0700

    Auto random input when no input given

--- Source/ARATesting.py
+++ Source/ARATesting.py
@@ -1,13 +1,21 @@
+import sys
+
 import Heuristic
 import RandomProblem
 import SolveProblem
 
 
 def main():
-    RandomProblem.createRandomProblem('rand_in.txt', 8, 16)
-    pf = SolveProblem.ARA('rand_in.txt', 'rand_log.txt', 3,
-                          Heuristic.EuclidDistance, 0.05)
-    pf.writeSolution('rand_out.txt')
+    # auto random file if no input
+    if len(sys.argv) != 3:
+        RandomProblem.createRandomProblem('rand_in.txt', 8, 16)
+        pf = SolveProblem.ARA('rand_in.txt', 'rand_log.txt', 3,
+                              Heuristic.EuclidDistance, 0.05)
+        pf.writeSolution('rand_out.txt')
+    else:
+        pf = SolveProblem.ARA(sys.argv[1], 'ARA_log.txt', 3,
+                              Heuristic.EuclidDistance, 0.05)
+        pf.writeSolution(sys.argv[2])
 
 
 if __name__ == '__main__':
--- Source/GUITesting.py
+++ Source/GUITesting.py
@@ -1,12 +1,19 @@
+import sys
+
 import RandomProblem
 import Heuristic
 import GUIDraw
 
 
 def main():
-    RandomProblem.createRandomProblem('rand_in.txt', 12, 24)
-    g = GUIDraw.GUIAStar('rand_in.txt', 'rand_log.txt')
-    g.draw(Heuristic.EuclidDistance)
+    # auto random file if no input
+    if len(sys.argv) != 2:
+        RandomProblem.createRandomProblem('rand_in.txt', 12, 24)
+        g = GUIDraw.GUIAStar('rand_in.txt', 'rand_log.txt')
+        g.draw(Heuristic.EuclidDistance)
+    else:
+        g = GUIDraw.GUIAStar(sys.argv[1], 'GUI_log.txt')
+        g.draw(Heuristic.EuclidDistance)
 
 
 if __name__ == '__main__':
