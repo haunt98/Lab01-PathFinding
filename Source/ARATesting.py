473c744ad63e3628e37d81655c68996bdbee32e7
Nguyen Tran Hau <ngtranhau@gmail.com>
2018-10-28 00:44:14 +0700

    For user input time limit

--- Source/ARATesting.py
+++ Source/ARATesting.py
@@ -7,14 +7,14 @@ import SolveProblem
 
 def main():
     # auto random file if no input
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         RandomProblem.createRandomProblem('rand_in.txt', 8, 16)
         pf = SolveProblem.ARA('rand_in.txt', 'rand_log.txt', 3,
                               Heuristic.EuclidDistance, 5)
         pf.writeSolution('rand_out.txt')
     else:
         pf = SolveProblem.ARA(sys.argv[1], 'ARA_log.txt', 3,
-                              Heuristic.EuclidDistance, 5)
+                              Heuristic.EuclidDistance, int(sys.argv[3]))
         pf.writeSolution(sys.argv[2])
 
 
